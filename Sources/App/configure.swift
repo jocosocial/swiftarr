33c6c88dffc30a4bd00eea9f430677e071481964
grundev <grundev@prosedev.com>
2019-10-23 18:35:01 -0700

    configure PostgreSQL connection

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -29,7 +29,28 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
 
     /// configure PostgreSQL connection
-
+    let postgresHostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let postgreslUser = Environment.get("DATABASE_USER") ?? "swiftarr"
+    let postgresPassword = Environment.get("DATABASE_PASSWORD") ?? "password"
+    let postgresDB: String
+    let postgresPort: Int
+    if (env == .testing) {
+        postgresDB = "swiftarr-test"
+        postgresPort = Int(Environment.get("DATABASE_PORT") ?? "5433")!
+    } else {
+        postgresDB = Environment.get("DATABASE_DB") ?? "swiftarr"
+        postgresPort = 5432
+    }
+    let postgresConfig = PostgreSQLDatabaseConfig(
+        hostname: postgresHostname,
+        port: postgresPort,
+        username: postgreslUser,
+        database: postgresDB,
+        password: postgresPassword,
+        transport: .cleartext
+    )
+    let postgres = PostgreSQLDatabase(config: postgresConfig)
+    
     /// configure Redis connection
     
     /// register databases
