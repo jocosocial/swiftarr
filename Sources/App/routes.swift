2b6615da4c20f0a04d163229075ec14529a534cd
grundev <grundev@prosedev.com>
2019-11-16 23:27:41 -0800

    add UsersController

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -18,4 +18,7 @@ public func routes(_ router: Router) throws {
     
     let userController = UserController()
     try router.register(collection: userController)
+    
+    let usersController = UsersController()
+    try router.register(collection: usersController)
 }
