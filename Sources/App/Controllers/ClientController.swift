99c11c35cdd93d5982623e47b737c41338c46842
grundev <grundev@prosedev.com>
2019-11-21 19:01:19 -0800

    add userUpdatesHandler + test

--- Sources/App/Controllers/ClientController.swift
+++ Sources/App/Controllers/ClientController.swift
@@ -32,7 +32,7 @@ struct ClientController: RouteCollection {
         // endpoints available whether logged in or out
 
         // endpoints available only when logged in
-        
+        tokenAuthGroup.get("user", "updates", "since", String.parameter, use: userUpdatesHandler)
     }
     
     // MARK: - Open Access Handlers
@@ -49,9 +49,43 @@ struct ClientController: RouteCollection {
     // All handlers in this route group require a valid HTTP Bearer Authentication
     // header in the request.
     
+    /// `GET /api/v3/client/user/updates/since/DATE`
+    ///
+    /// Retrieves the `User.Public` of all users with a `.profileUpdatedAt` timestamp later
+    /// than the specified date. The `DATE` parameter is a string, and may be provided in either
+    /// of two formats:
+    ///
+    /// * a string literal `Double` (e.g. "1574364935" or "1574364935.88991")
+    /// * a UTC ISO8601 "yyyy-MM-dd'T'HH:mm:ssZ" string (e.g. "2019-11-21T05:31:28Z"), the
+    ///   date format returned in `swiftarr` JSON responses
+    ///
+    /// - Parameter req: The incoming request `Container`, provided automatically.
+    /// - Throws: 400 error if no valid date string provided. 403 error if user is not
+    ///   a registered client.
+    /// - Returns: Array of all updated users, as `User.Public` objects.
+    func userUpdatesHandler(_ req: Request) throws -> Future<[User.Public]> {
+        let user = try req.requireAuthenticated(User.self)
+        // must be registered client
+        guard user.accessLevel == .client else {
+            throw Abort(.forbidden, reason: "registered clients only")
+        }
+        // parse date parameter
+        let since = try req.parameters.next(String.self)
+        guard let date = ClientController.dateFromParameter(string: since) else {
+            throw Abort(.badRequest, reason: "'\(since)' is not a recognized date format")
+        }
+        // return .Public array
+        return User.query(on: req)
+            .filter(\.profileUpdatedAt > date)
+            .all()
+            .map {
+                (users) in
+                let publicUsers = try users.map { try $0.convertToPublic() }
+                return publicUsers
+        }
+    }
+    
+    // MARK: - Helper Functions
 }
 
-// MARK: - Helper Functions
-
 // MARK: - Helper Structs
-
--- Tests/AppTests/ClientTests.swift
+++ Tests/AppTests/ClientTests.swift
@@ -66,5 +66,99 @@ final class ClientTests: XCTestCase {
         )
         XCTAssertFalse(recoveryresult.token.isEmpty, "should receive valid token string")
     }
+    
+    /// `GET /api/v3/client/user/updates/since/DATE`
+    func testUserUpdates() throws {
+        // create logged in client
+        var token = try app.login(username: testClientname, password: testPassword, on: conn)
+        var headers = HTTPHeaders()
+        headers.bearerAuthorization = BearerAuthorization(token: token.token)
+        
+        // test updates since -1
+        var users = try app.getResult(
+            from: clientURI + "user/updates/since/-1",
+            method: .GET,
+            headers: headers,
+            decodeTo: [User.Public].self
+        )
+        XCTAssertTrue(users.count > 1, "should be all accounts")
+        
+        // create update
+        var currentDate = Date()
+        let userProfileData = UserProfileData(
+            about: "",
+            displayName: "Client User",
+            email: "",
+            homeLocation: "",
+            message: "",
+            preferredPronoun: "",
+            realName: "",
+            roomNumber: "",
+            limitAccess: false
+        )
+        var response = try app.getResponse(
+            from: userURI + "profile",
+            method: .POST,
+            headers: headers,
+            body: userProfileData
+        )
+        XCTAssertTrue(response.http.status.code == 200, "should be 200 OK")
+        
+        // test updates with Double
+        users = try app.getResult(
+            from: clientURI + "user/updates/since/\(currentDate.timeIntervalSince1970)",
+            method: .GET,
+            headers: headers,
+            decodeTo: [User.Public].self
+        )
+        XCTAssertTrue(users.count == 1, "should be 1 updated account")
+
+        // test updates with 8601 string
+        var isoString = ""
+        if #available(OSX 10.12, *) {
+            isoString = ISO8601DateFormatter().string(from: currentDate)
+        } else {
+            // Fallback on earlier versions
+        }
+        users = try app.getResult(
+            from: clientURI + "user/updates/since/\(isoString)",
+            method: .GET,
+            headers: headers,
+            decodeTo: [User.Public].self
+        )
+        XCTAssertTrue(users.count == 1, "should be 1 updated account")
+        
+        // test no updates
+        sleep(1)
+        currentDate = Date()
+        users = try app.getResult(
+            from: clientURI + "user/updates/since/\(currentDate.timeIntervalSince1970)",
+            method: .GET,
+            headers: headers,
+            decodeTo: [User.Public].self
+        )
+        XCTAssertTrue(users.count == 0, "should be no updated accounts")
+
+        // test bad date
+        response = try app.getResponse(
+            from: clientURI + "user/updates/since/GARBAGE",
+            method: .GET,
+            headers: headers
+        )
+        XCTAssertTrue(response.http.status.code == 400, "should be 400 Bad Request")
+        XCTAssertTrue(response.http.body.description.contains("not a recognized"), "not a recognized")
+        
+        // test not a client
+        _ = try app.createUser(username: testUsername, password: testPassword, on: conn)
+        token = try app.login(username: testUsername, password: testPassword, on: conn)
+        headers.bearerAuthorization = BearerAuthorization(token: token.token)
+        response = try app.getResponse(
+            from: clientURI + "user/updates/since/\(currentDate.timeIntervalSince1970)",
+            method: .GET,
+            headers: headers
+        )
+        XCTAssertTrue(response.http.status.code == 403, "should be 403 Bad Request")
+        XCTAssertTrue(response.http.body.description.contains("clients only"), "clients only")
+    }
 }
 
