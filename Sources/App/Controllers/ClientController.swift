31ba757fd25bff1ea03e3f41a564078a552f1d5f
grundev <grundev@prosedev.com>
2019-11-21 23:06:39 -0800

    add userHeadersHandler

--- Sources/App/Controllers/ClientController.swift
+++ Sources/App/Controllers/ClientController.swift
@@ -32,6 +32,7 @@ struct ClientController: RouteCollection {
         // endpoints available whether logged in or out
 
         // endpoints available only when logged in
+        tokenAuthGroup.get("user", "headers", "since", String.parameter, use: userHeadersHandler)
         tokenAuthGroup.get("user", "updates", "since", String.parameter, use: userUpdatesHandler)
     }
     
@@ -49,6 +50,44 @@ struct ClientController: RouteCollection {
     // All handlers in this route group require a valid HTTP Bearer Authentication
     // header in the request.
     
+    /// `GET /api/v3/client/user/headers/since/DATE`
+    ///
+    /// Retrieves the `UserProfile.Header` of all users with a `.profileUpdatedAt` timestamp
+    /// later than the specified date. The `DATE` parameter is a string, and may be provided
+    /// in either of two formats:
+    ///
+    /// * a string literal `Double` (e.g. "1574364935" or "1574364935.88991")
+    /// * an ISO 8601 `yyyy-MM-dd'T'HH:mm:ssZ` string (e.g. "2019-11-21T05:31:28Z")
+    ///
+    /// The second format is precisely what is returned in `swiftarr` JSON responses, while
+    /// the numeric form makes for a prettier URL.
+    ///
+    /// - Parameter req: The incoming request `Container`, provided automatically.
+    /// - Throws: 400 error if no valid date string provided. 403 error if user is not
+    ///   a registered client.
+    /// - Returns: Array of all updated users, as `UserProfile.Header` objects.
+    func userHeadersHandler(_ req: Request) throws -> Future<[UserProfile.Header]> {
+        let user = try req.requireAuthenticated(User.self)
+        // must be registered client
+        guard user.accessLevel == .client else {
+            throw Abort(.forbidden, reason: "registered clients only")
+        }
+        // parse date parameter
+        let since = try req.parameters.next(String.self)
+        guard let date = ClientController.dateFromParameter(string: since) else {
+            throw Abort(.badRequest, reason: "'\(since)' is not a recognized date format")
+        }
+        // return .Header array
+        return UserProfile.query(on: req)
+            .filter(\.updatedAt > date)
+            .all()
+            .map {
+                (profiles) in
+                let headers = try profiles.map { try $0.convertToHeader() }
+                return headers
+        }
+    }
+
     /// `GET /api/v3/client/user/updates/since/DATE`
     ///
     /// Retrieves the `User.Public` of all users with a `.profileUpdatedAt` timestamp later
