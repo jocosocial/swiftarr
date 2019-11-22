30668d981dfe8b73ea21b514864b1d5df5ec052b
grundev <grundev@prosedev.com>
2019-11-21 18:55:51 -0800

    move +Extensions files to own directory

---
+++ Sources/App/Extensions/RouteCollection+Extensions.swift
@@ -0,0 +1,34 @@
+import Foundation
+import Vapor
+
+extension RouteCollection {
+    
+    /// Transforms a string that might represent a date (either a `Double` or an ISO 8601
+    /// representation) into a `Date`, if possible.
+    ///
+    /// - Note: The representation is expected to be either a string literal `Double`, or a
+    ///   string in UTC `yyyy-MM-dd'T'HH:mm:ssZ` format.
+    ///
+    /// - Parameter string: The string to be transformed.
+    /// - Returns: A `Date` if the conversion was successful, otherwise `nil`.
+    static func dateFromParameter(string: String) -> Date? {
+        var date: Date?
+        if let timeInterval = TimeInterval(string) {
+            date = Date(timeIntervalSince1970: timeInterval)
+        } else {
+            if #available(OSX 10.12, *) {
+                if let dateFromISO8601 = ISO8601DateFormatter().date(from: string) {
+                    date = dateFromISO8601
+                }
+            } else {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+                if let dateFromDateFormatter = dateFormatter.date(from: string) {
+                    date = dateFromDateFormatter
+                }
+            }
+        }
+        return date
+    }
+}
+
