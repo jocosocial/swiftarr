c6f85035bf3356a2a4b2acc14347dae26b6d8a22
grundev <grundev@prosedev.com>
2019-11-25 15:28:19 -0800

    refactor cases

--- Sources/App/Models/BarrelType.swift
+++ Sources/App/Models/BarrelType.swift
@@ -3,12 +3,12 @@ import FluentPostgreSQL
 /// The type of `Barrel`.
 
 enum BarrelType: String, PostgreSQLRawEnum {
+    /// A user's barrel of muting keywords.
+    case keywordMute
     /// A generic barrel of seamonkeys.
     case seamonkey
     /// A user's barrel of blocked seamonkeys.
     case userBlock
     /// A user's barrel of muted seamonkeys.
     case userMute
-    /// A user's barrel of muting keywords.
-    case wordMute
 }
