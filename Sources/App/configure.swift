1296ab740b44f60d72307173e5967bb13060c899
grundev <grundev@prosedev.com>
2019-12-01 16:18:54 -0800

    add Report migration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -103,6 +103,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     migrations.add(model: ProfileEdit.self, database: .psql)
     migrations.add(model: UserNote.self, database: .psql)
     migrations.add(model: Barrel.self, database: .psql)
+    migrations.add(model: Report.self, database: .psql)
     migrations.add(migration: AdminUser.self, database: .psql)
     migrations.add(migration: ClientUsers.self, database: .psql)
     migrations.add(migration: RegistrationCodes.self, database: .psql)
