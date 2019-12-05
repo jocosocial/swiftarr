8ffbba100d89393089acf93c3e7868ae6f45faed
grundev <grundev@prosedev.com>
2019-12-04 22:35:28 -0800

    edit docs

--- Sources/App/Models/Category.swift
+++ Sources/App/Models/Category.swift
@@ -2,8 +2,8 @@ import Vapor
 import FluentPostgreSQL
 
 /// Categories are used to organize Twit-arr `Forum`s into a managable structure. All `Forum`s
-/// belong to a single `Category`. A category is classified as one of two types: "restricted"
-/// (an "official" set of forums) or "user" (users can create forums).
+/// belong to a single `Category`. A category is classified as one of two types: "admin"
+/// (an administratively controlled set of forums) or "user" (users can create forums).
 
 final class Category: Codable {
     // MARK: Properties
