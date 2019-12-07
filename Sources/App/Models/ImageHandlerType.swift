39b004912f3aed58d4e03a1ab01b99450e25ff71
grundev <grundev@prosedev.com>
2019-12-06 18:24:27 -0800

    add ImageHandlerType enum

---
+++ Sources/App/Models/ImageHandlerType.swift
@@ -0,0 +1,13 @@
+import FluentPostgreSQL
+
+/// The type of model for which an `ImageHandler` is processing. This defines the
+/// size of the thumbnail produced.
+
+enum ImageHandlerType: String, PostgreSQLRawEnum {
+    /// The image is for a `ForumPost`.
+    case forumPost
+    /// The image is for a `Twarrt`
+    case twarrt
+    /// The image is for a `User`'s profile.
+    case userProfile
+}
