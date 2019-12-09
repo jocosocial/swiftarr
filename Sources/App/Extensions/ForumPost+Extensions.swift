43fb0dbd090df1e6a07c8a332a537a974525dae2
grundev <grundev@prosedev.com>
2019-12-08 22:15:16 -0800

    add convertToData

--- Sources/App/Extensions/ForumPost+Extensions.swift
+++ Sources/App/Extensions/ForumPost+Extensions.swift
@@ -47,3 +47,28 @@ extension ForumPost {
         return parent(\.forumID)
     }
 }
+
+// MARK: - Functions
+
+extension ForumPost {
+    /// Converts an `ForumPost` model to a version omitting data that is of no interest to a user.
+    func convertToData() -> ForumPostData {
+        return ForumPostData(
+            text: self.text,
+            image: self.image
+        )
+    }
+}
+
+extension Future where T: ForumPost {
+    /// Converts a `Future<ForumPost>` to a `Future<ForumPostData>`. This extension provides
+    /// the convenience of simply using `event.convertToData()` and allowing the compiler to
+    /// choose the appropriate version for the context.
+    func convertToData() -> Future<ForumPostData> {
+        return self.map {
+            (forumPost) in
+            return forumPost.convertToData()
+        }
+    }
+}
+
