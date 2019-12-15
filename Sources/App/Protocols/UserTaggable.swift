ae3c4c689cfeb55b12af06f94222f5dcd9312218
grundev <grundev@prosedev.com>
2019-12-15 13:59:43 -0800

    add UserTaggable protocol

---
+++ Sources/App/Protocols/UserTaggable.swift
@@ -0,0 +1,32 @@
+import Vapor
+import FluentPostgreSQL
+
+/// A `Protocol` used to provide convenience functions for RouteCollection controllers that
+/// return content that can be tagged by the user on a per-model basis.
+
+protocol UserTaggable {
+    /// The barrel type to be used for tagged model ID storage.
+    var taggedBarrelType: BarrelType { get }
+}
+
+extension UserTaggable {
+    /// Returns the `Barrel` of type `.taggedarrelType` for the request's `User`, or nil
+    /// if none exists.
+    ///
+    /// - Parameters:
+    ///   - user: The user who owns the barrel.
+    ///   - req: The incoming `Request`, on whose event loop this must run.
+    func getTaggedBarrel(for user: User, on req: Request) throws -> Future<Barrel?> {
+        return try Barrel.query(on: req)
+            .filter(\.ownerID == user.requireID())
+            .filter(\.barrelType == taggedBarrelType)
+            .first()
+            .map {
+                (barrel) in
+                guard let barrel = barrel else {
+                    return nil
+                }
+                return barrel
+        }
+    }
+}
