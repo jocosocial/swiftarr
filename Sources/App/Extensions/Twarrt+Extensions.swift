e9816d097c882dbdc5888b03d4bb460479878e0b
grundev <grundev@prosedev.com>
2019-12-16 19:05:42 -0800

    add Twarrt, Future<Twarrt> convertToData functions

--- Sources/App/Extensions/Twarrt+Extensions.swift
+++ Sources/App/Extensions/Twarrt+Extensions.swift
@@ -33,3 +33,30 @@ extension Twarrt {
 }
 
 // MARK: - Functions
+
+extension Twarrt {
+    /// Converts a `Twarrt` model to a version omitting data that is of no interest to a user.
+    func convertToData(withLike userLike: LikeType?, likeCount: Int) throws -> PostData {
+        return try PostData(
+            postID: self.requireID(),
+            createdAt: self.createdAt ?? Date(),
+            authorID: self.authorID,
+            text: self.text,
+            image: self.image,
+            userLike: userLike,
+            likeCount: likeCount
+        )
+    }
+}
+
+extension Future where T: Twarrt {
+    /// Converts a `Future<Twarrt>` to a `Future<PostData>`. This extension provides
+    /// the convenience of simply using `twarrt.convertToData()` and allowing the compiler to
+    /// choose the appropriate version for the context.
+    func convertToData(withLike userLike: LikeType?, likeCount: Int) -> Future<PostData> {
+        return self.map {
+            (twarrt) in
+            return try twarrt.convertToData(withLike: userLike, likeCount: likeCount)
+        }
+    }
+}
