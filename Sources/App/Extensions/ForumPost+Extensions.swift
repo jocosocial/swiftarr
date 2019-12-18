3525c09018cc89b390c76ca21c7859648c0e81bc
grundev <grundev@prosedev.com>
2019-12-17 19:10:38 -0800

    update convertToData extensions for bookmarks

--- Sources/App/Extensions/ForumPost+Extensions.swift
+++ Sources/App/Extensions/ForumPost+Extensions.swift
@@ -62,13 +62,14 @@ extension ForumPost {
 
 extension ForumPost {
     /// Converts an `ForumPost` model to a version omitting data that is of no interest to a user.
-    func convertToData(withLike userLike: LikeType?, likeCount: Int) throws -> PostData {
+    func convertToData(bookmarked: Bool, userLike: LikeType?, likeCount: Int) throws -> PostData {
         return try PostData(
             postID: self.requireID(),
             createdAt: self.createdAt ?? Date(),
             authorID: self.authorID,
             text: self.text,
             image: self.image,
+            isBookmarked: bookmarked,
             userLike: userLike,
             likeCount: likeCount
         )
@@ -79,10 +80,14 @@ extension Future where T: ForumPost {
     /// Converts a `Future<ForumPost>` to a `Future<PostData>`. This extension provides
     /// the convenience of simply using `post.convertToData()` and allowing the compiler to
     /// choose the appropriate version for the context.
-    func convertToData(withLike userLike: LikeType?, likeCount: Int) -> Future<PostData> {
+    func convertToData(bookmarked: Bool, userLike: LikeType?, likeCount: Int) -> Future<PostData> {
         return self.map {
             (forumPost) in
-            return try forumPost.convertToData(withLike: userLike, likeCount: likeCount)
+            return try forumPost.convertToData(
+                bookmarked: bookmarked,
+                userLike: userLike,
+                likeCount: likeCount
+            )
         }
     }
 }
--- Sources/App/Extensions/Twarrt+Extensions.swift
+++ Sources/App/Extensions/Twarrt+Extensions.swift
@@ -41,13 +41,14 @@ extension Twarrt {
 
 extension Twarrt {
     /// Converts a `Twarrt` model to a version omitting data that is of no interest to a user.
-    func convertToData(withLike userLike: LikeType?, likeCount: Int) throws -> PostData {
+    func convertToData(bookmarked: Bool, userLike: LikeType?, likeCount: Int) throws -> PostData {
         return try PostData(
             postID: self.requireID(),
             createdAt: self.createdAt ?? Date(),
             authorID: self.authorID,
             text: self.text,
             image: self.image,
+            isBookmarked: bookmarked,
             userLike: userLike,
             likeCount: likeCount
         )
@@ -58,10 +59,14 @@ extension Future where T: Twarrt {
     /// Converts a `Future<Twarrt>` to a `Future<PostData>`. This extension provides
     /// the convenience of simply using `twarrt.convertToData()` and allowing the compiler to
     /// choose the appropriate version for the context.
-    func convertToData(withLike userLike: LikeType?, likeCount: Int) -> Future<PostData> {
+    func convertToData(bookmarked: Bool, userLike: LikeType?, likeCount: Int) -> Future<PostData> {
         return self.map {
             (twarrt) in
-            return try twarrt.convertToData(withLike: userLike, likeCount: likeCount)
+            return try twarrt.convertToData(
+                bookmarked: bookmarked,
+                userLike: userLike,
+                likeCount: likeCount
+            )
         }
     }
 }
