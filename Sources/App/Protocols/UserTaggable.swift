f8af7b95f79f4687ad20f54e2654ef4f972db618
grundev <grundev@prosedev.com>
2019-12-17 19:20:22 -0800

    refactor UserTaggable type

--- Sources/App/Protocols/UserTaggable.swift
+++ Sources/App/Protocols/UserTaggable.swift
@@ -6,20 +6,21 @@ import FluentPostgreSQL
 
 protocol UserTaggable {
     /// The barrel type to be used for tagged model ID storage.
-    var taggedBarrelType: BarrelType { get }
+    var favoriteBarrelType: BarrelType { get }
 }
 
 extension UserTaggable {
-    /// Returns the `Barrel` of type `.taggedarrelType` for the request's `User`, or nil
+    /// Returns the `Barrel` of type `.taggedBarrelType` for the request's `User`, or nil
     /// if none exists.
     ///
     /// - Parameters:
     ///   - user: The user who owns the barrel.
     ///   - req: The incoming `Request`, on whose event loop this must run.
+    /// - Returns: `Barrel` of the required type, or `nil`.
     func getTaggedBarrel(for user: User, on req: Request) throws -> Future<Barrel?> {
         return try Barrel.query(on: req)
             .filter(\.ownerID == user.requireID())
-            .filter(\.barrelType == taggedBarrelType)
+            .filter(\.barrelType == favoriteBarrelType)
             .first()
             .map {
                 (barrel) in
