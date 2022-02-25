8b7d302f0dab6f47c0df2a86a26f59e9c2eed075
Chall Fry <chall@challfry.com>
2022-02-25 15:30:15 -0800

    Removing Fez custom emoji.
    Per Storm's request.

--- Sources/App/Site/CustomLeafTags.swift
+++ Sources/App/Site/CustomLeafTags.swift
@@ -48,7 +48,7 @@ import Foundation
 ///
 /// Usage: #addJocomoji(String) -> String
 struct AddJocomojiTag: UnsafeUnescapedLeafTag {
-	static let jocomoji = [ "buffet", "die-ship", "die", "fez", "hottub", "joco", "pirate", "ship-front",
+	static let jocomoji = [ "buffet", "die-ship", "die", "hottub", "joco", "pirate", "ship-front",
 			"ship", "towel-monkey", "tropical-drink", "zombie" ]
 			
 	static func process(_ str: String) -> String {
