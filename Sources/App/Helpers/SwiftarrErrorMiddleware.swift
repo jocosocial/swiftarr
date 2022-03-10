a4ca4e863eb9986f6fd774a6d0f50be614eb123b
Chall Fry <chall@challfry.com>
2022-03-10 02:50:32 -0500

    Added more debug logging.
    Requests now get logged both the start and end of the request (in errorMiddleware).

--- Sources/App/Helpers/SwiftarrErrorMiddleware.swift
+++ Sources/App/Helpers/SwiftarrErrorMiddleware.swift
@@ -65,8 +65,9 @@ public final class SwiftarrErrorMiddleware: Middleware {
 
 	/// See `Middleware`.
 	public func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
+		request.logger.log(level: .info, "START \(request.method) \(request.url.path.removingPercentEncoding ?? request.url.path)")
 		return next.respond(to: request).map { response in
-			request.logger.log(level: .info, "\(request.method) \(request.url.path.removingPercentEncoding ?? request.url.path) -> \(response.status)")
+			request.logger.log(level: .info, "END   \(request.method) \(request.url.path.removingPercentEncoding ?? request.url.path) -> \(response.status)")
 			return response
 		}.flatMapErrorThrowing { error in
 			return self.handleError(req: request, error: error)
