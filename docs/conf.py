26801b5fd7ee877e143eee6a3097040cebd395f5
Grant Cohoe <grant@grantcohoe.com>
2023-02-23 11:22:49 -0500

    Purge generated docs (#145)

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # Project information
 project = 'Swiftarr'
 author = 'The Twitarr Team'
-copyright = '2022, The Twitarr Team'
+copyright = '2023, The Twitarr Team'
 
 # Enables writing docs in Markdown
 extensions = ['myst_parser']
