6bdf79395e09c443cab7247346add9488d7f7da5
Grant Cohoe <grant@grantcohoe.com>
2023-03-19 12:20:22 -0400

    format top level stuff

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,44 +8,44 @@ public func routes(_ app: Application) throws {
 	//
 	// API routes generally use Tokens to auth, do not use sessions, and use Fluent and Redis to access the underlying databases.
 	let apiControllers: [APIRouteCollection] = [
-			AdminController(),
-			ModerationController(),
-			AlertController(),
-			AuthController(),
-			BoardgameController(),
-			ClientController(),
-			EventController(),
-			FezController(),
-			ForumController(),
-			ImageController(),
-			KaraokeController(),
-			TestController(),
-			TwitarrController(),
-			UserController(),
-			UsersController(),
-			PhonecallController(),
+		AdminController(),
+		ModerationController(),
+		AlertController(),
+		AuthController(),
+		BoardgameController(),
+		ClientController(),
+		EventController(),
+		FezController(),
+		ForumController(),
+		ImageController(),
+		KaraokeController(),
+		TestController(),
+		TwitarrController(),
+		UserController(),
+		UsersController(),
+		PhonecallController(),
 	]
 	try apiControllers.forEach { try $0.registerRoutes(app) }
 
-	// Site Route Controllers handle 'GET' routes that return HTML and 'POST' routes that take data from HTML forms. 
+	// Site Route Controllers handle 'GET' routes that return HTML and 'POST' routes that take data from HTML forms.
 	// Site Routes use session cookies to track user sessions. Site routes (generally) don't access the DB directly,
-	// instead calling API routes using Vapor's Client APIs to access model data. 
+	// instead calling API routes using Vapor's Client APIs to access model data.
 	//
 	// API tokens are stored in session data, allowing site routes to make authenticated calls to API routes.
 	let siteControllers: [SiteControllerUtils] = [
-			SiteFileController(),
-			SiteController(),
-			SiteLoginController(),
-			SiteTwitarrController(),
-			SiteSeamailController(),
-			SiteFriendlyFezController(),
-			SiteForumController(),
-			SiteEventsController(),
-			SiteUserController(),
-			SiteBoardgameController(),
-			SiteKaraokeController(),
-			SiteModController(),
-			SiteAdminController(),
+		SiteFileController(),
+		SiteController(),
+		SiteLoginController(),
+		SiteTwitarrController(),
+		SiteSeamailController(),
+		SiteFriendlyFezController(),
+		SiteForumController(),
+		SiteEventsController(),
+		SiteUserController(),
+		SiteBoardgameController(),
+		SiteKaraokeController(),
+		SiteModController(),
+		SiteAdminController(),
 	]
 	try siteControllers.forEach { try $0.registerRoutes(app) }
 }
