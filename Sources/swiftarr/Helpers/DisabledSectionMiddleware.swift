0d3e64388e8c232f9c331b8876f13442d25d63e6
Grant Cohoe <grant@grantcohoe.com>
2023-12-17 13:32:17 -0500

    resolve #133, change disabled feature return code response

--- Sources/swiftarr/Helpers/DisabledSectionMiddleware.swift
+++ Sources/swiftarr/Helpers/DisabledSectionMiddleware.swift
@@ -11,7 +11,11 @@ struct DisabledAPISectionMiddleware: AsyncMiddleware {
 	func respond(to request: Request, chainingTo next: AsyncResponder) async throws -> Response {
 		let features = Settings.shared.disabledFeatures.value
 		if features[.all]?.contains(featureToCheck) ?? false || features[.all]?.contains(.all) ?? false {
-			throw Abort(.serviceUnavailable)
+			// There are a couple StackOverflow posts discussing what code is most appropriate
+			// for a "correct request but the server refuses to process it" situation like this.
+			// The 451 Legal Reasons code is so far out out there that I think we're in safe
+			// position to use that as the code for "we've disabled this feature".
+			throw Abort(.unavailableForLegalReasons, reason: "This feature has been disabled by the server admins.")
 		}
 		return try await next.respond(to: request)
 	}
