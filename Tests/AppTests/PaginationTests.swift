b625421619461a35d46db0da3ecdff5dc787d660
Victor Solano <victor.solanonunez@gmail.com>
2026-07-19 13:13:17 +0200

    test: cover pagination query bounds

---
+++ Tests/AppTests/PaginationTests.swift
@@ -0,0 +1,36 @@
+import XCTVapor
+@testable import swiftarr
+
+class PaginationTests: XCTestCase {
+
+	func testStartDefaultsToZero() {
+		XCTAssertEqual(Pagination.start(nil), 0)
+	}
+
+	func testStartClampsNegativeValues() {
+		XCTAssertEqual(Pagination.start(-1), 0)
+		XCTAssertEqual(Pagination.start(nil, default: -5), 0)
+	}
+
+	func testStartPreservesPositiveValues() {
+		XCTAssertEqual(Pagination.start(12), 12)
+	}
+
+	func testLimitDefaultsToFifty() {
+		XCTAssertEqual(Pagination.limit(nil, maximum: 200), 50)
+	}
+
+	func testLimitClampsToNonzeroLowerBound() {
+		XCTAssertEqual(Pagination.limit(0, maximum: 200), 1)
+		XCTAssertEqual(Pagination.limit(-10, maximum: 200), 1)
+		XCTAssertEqual(Pagination.limit(nil, default: 0, maximum: 200), 1)
+	}
+
+	func testLimitClampsToConfiguredMaximum() {
+		XCTAssertEqual(Pagination.limit(201, maximum: 200), 200)
+	}
+
+	func testLimitPreservesInRangeValues() {
+		XCTAssertEqual(Pagination.limit(25, maximum: 200), 25)
+	}
+}
